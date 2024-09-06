ddc857f37c08c97e9cefccd3da04942bcaecab7e
AzamatZakirovch <a.eshqoziyev@newuu.uz>
2024-09-06 12:45:13 +0500

    I should work with streamlit.io so I need to write my code should be in python language. I wanted to read data set in .py.

---
+++ data_base.py
@@ -0,0 +1,17 @@
+def data():
+    import pandas as pd
+    # reading part
+    df = pd.read_csv('1 copy.csv')
+
+    # in .ipynb file I did it important works so i copied all of them in to python file!
+
+    to_int = ["Investments (USD)", "GDP", "R&D Expenditure", "Energy Subsidies", "International Aid for Renewables"]
+
+    to_bool = ["Government Policies", "Renewable Energy Targets", "Energy Efficiency Programs",
+               "Energy Market Liberalization", "Technology Transfer Agreements", "Renewable Energy Education Programs",
+               "Natural Disasters", "Public-Private Partnerships in Energy", "Regional Renewable Energy Cooperation"]
+    df[to_int] = df[to_int].astype(int)
+    df[to_bool] = df[to_bool].astype(bool)
+
+    # everything is ready I've got database. I need to return it as .csv file
+    return df
