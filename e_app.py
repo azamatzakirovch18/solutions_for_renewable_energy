d977df97fce3492140e03b0c11a73e2fe082c6fa
AzamatZakirovch <a.eshqoziyev@newuu.uz>
2024-09-06 13:07:59 +0500

    In this file is very one of the most important part. With this file you can control different pages

---
+++ e_app.py
@@ -0,0 +1,40 @@
+# In this file, I will connect all pages into main page
+
+# those are important libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import streamlit as st
+
+# those are pages
+from f_introduction import introduction
+from g_analysis import analysis
+from problems_and_solutions import problemsAndSolutions
+
+# lets get started
+st.set_page_config(
+    page_title="Global Renewable Dashboard",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+# with this function I can create buttons for different pages
+with st.sidebar:
+    options = st.selectbox(
+        "please choose you oprion",
+        options=[
+            "Main Page",
+            "Analysis Page",
+            "Solutions Page"
+        ]
+
+    )
+
+# That's part of working pages
+if options == "Main Page":
+    introduction()
+elif options == "Analysis Page":
+    analysis()
+elif options == "Solutions Page":
+    problemsAndSolutions()
