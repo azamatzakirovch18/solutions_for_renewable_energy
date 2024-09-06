41ee1c315d22e368ed4543ff4bd54f1487e3f0f0
AzamatZakirovch <a.eshqoziyev@newuu.uz>
2024-09-06 23:26:04 +0500

    Introduction part is finished

--- f_introduction.py
+++ f_introduction.py
@@ -1,5 +1,30 @@
 def introduction():
     import streamlit as st
 
+    # st.write("## Welcome to my project")
+    st.write("## Renewable Energy")
+    st.write("""
+    This project is based on Global Renewable energy and includes 10 countries. There are 56 columns for representation.
+    I divided then into 4 groups for analysing. They are :
+    - Renewable Energy Data
+    - Socio-Economic Indicators:
+    - Environmental Factors
+    - Additional Features
+    ### Most important columns
+    - Year
+    - Country
+    - Type of Renewable Energy
+    - Government Policies
+    
+    Those columns are one of the most important for analysing. I can seperate them between 2 years by type of energy. Private or Primary. 
+    ### Problems & Solution
+    During this project i used only 1 question "WHY?" and it is helped to me find a lot of problems and solution for problems !
+    
+    
+    ____________________________________________________________________________________________________________________
+    More information about US [@azamatzakirovch](https://www.instagram.com/azamatzakirovch)\n
+    
+    Tashkent 2024
+    """)
 
-    st.write("This is introduction")
+    # introduction part is finished
